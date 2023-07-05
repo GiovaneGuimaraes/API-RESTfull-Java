68b9600c56015fd0dc123c6ddc31f658a65cf651
GiovaneGuimaraes <133304083+GiovaneGuimaraes@users.noreply.github.com>
2023-07-05 17:24:21 -0300

    Update EspecialidadeDAO.java

--- hospitalJava/untitled/src/data/EspecialidadeDAO.java
+++ hospitalJava/untitled/src/data/EspecialidadeDAO.java
@@ -4,13 +4,14 @@ import model.Especialidade;
 
 import java.util.List;
 
-public interface EspecialidadeDAO {
+public interface EspecialidadeDAO extends DAO<Especialidade>{
 
+    @Override
     void salvar(Especialidade especialidade);
 
-
+    @Override
     Especialidade buscar(int id);
 
-
+    @Override
     List<Especialidade> buscarTodos();
 }
