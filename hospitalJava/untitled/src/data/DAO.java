15aa54bedd364cbcac4fb6b3a201af79171d9df1
GiovaneGuimaraes <133304083+GiovaneGuimaraes@users.noreply.github.com>
2023-07-05 17:24:06 -0300

    Update DAO.java

--- hospitalJava/untitled/src/data/DAO.java
+++ hospitalJava/untitled/src/data/DAO.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 public interface DAO <T>{
     void salvar(T type);
-    void atualizar(T type);
-    void apagar(T type);
     T buscar(int id);
     List<T>buscarTodos();
 }
